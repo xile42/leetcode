186e33da64ec546e1b1db8225acd73716fb08f31
xile42 <zpu@pku.edu.cn>
2025-05-07 23:59:59 +0800

    Update solution.

--- python3/464. 我能赢吗.py
+++ python3/464. 我能赢吗.py
@@ -2,4 +2,14 @@ class Solution:
 
     def canIWin(self, maxChoosableInteger: int, desiredTotal: int) -> bool:
 
-        return sum(list(range(1, maxChoosableInteger + 1))[::-1][::2]) >= desiredTotal
+        @cache
+        def dfs(usedNumbers: int, currentTotal: int) -> bool:
+
+            for i in range(maxChoosableInteger):
+                if (usedNumbers >> i) & 1 == 0:
+                    if currentTotal + i + 1 >= desiredTotal or not dfs(usedNumbers | (1 << i), currentTotal + i + 1):
+                        return True
+
+            return False
+
+        return (1 + maxChoosableInteger) * maxChoosableInteger // 2 >= desiredTotal and dfs(0, 0)
--- python3/695. 岛屿的最大面积.py
+++ python3/695. 岛屿的最大面积.py
@@ -2,10 +2,24 @@ class Solution:
 
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
 
+        ans = 0
         m, n = len(grid), len(grid[0])
-        vis = [[False] * n] * m
+        vis = [[False for _ in range(n)] for _ in range(m)]
 
-        def f(i, j):
+        def dfs(i, j):
 
-            pass
-        
+            vis[i][j] = True
+            ans = 1
+            for ii, jj in (i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1):
+                if 0 <= ii < m and 0 <= jj < n and grid[ii][jj] and not vis[ii][jj]:
+                    ans += dfs(ii, jj)
+
+            return ans
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] and not vis[i][j]:
+                    ans = max(ans, dfs(i, j))
+
+        return ans
+
