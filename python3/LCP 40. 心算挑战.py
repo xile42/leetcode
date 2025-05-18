a7aa7c79c2fb44686f88485bdf60c2690202d732
xile42 <zpu@pku.edu.cn>
2025-05-18 23:59:59 +0800

    Update solution.

--- python3/LCP 40. 心算挑战.py
+++ python3/LCP 40. 心算挑战.py
@@ -2,9 +2,22 @@ class Solution:
 
     def maximumScore(self, cards: List[int], cnt: int) -> int:
 
-        ans = 0
-        for i in permutations(cards, cnt):
-            if not (s := sum(i)) & 1:
-                ans = max(ans, s)
+        cards.sort(reverse=True)
+        s = sum(cards[:cnt])  # 最大的 cnt 个数之和
+        if s % 2 == 0:  # s 是偶数
+            return s
+
+        def replaced_sum(x: int) -> int:
+            for v in cards[cnt:]:
+                if v % 2 != x % 2:  # 找到一个最大的奇偶性和 x 不同的数
+                    return s - x + v  # 用 v 替换 s
+            return 0
+
+        x = cards[cnt - 1]
+        ans = replaced_sum(x)  # 替换 x
+        for v in cards[cnt - 1::-1]:
+            if v % 2 != x % 2:  # 找到一个最小的奇偶性和 x 不同的数
+                ans = max(ans, replaced_sum(v))  # 替换
+                break
 
         return ans
